2815b853d2cd0510a76dac4be5a255087fabd42c
zrakhimov <zokir.rakhimov@gmail.com>
2019-08-13 14:45:36 -0700

    add /chat route

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,4 +10,13 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html");
+    return render_template("index.html")
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        return render_template("chat.html")
+
+    elif request.method == "POST":
+        #Save nickname, display on every sent message
+        return "POST detected"
