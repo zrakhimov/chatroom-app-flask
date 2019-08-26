a4ce291ad10ce5e3d648265a08620a5042fb9e06
Rakhimov <zokir.rakhimov@gmail.com>
2019-08-26 15:39:45 -0400

    Pressing returnwill trigger add action (channel)

--- static/chat.js
+++ static/chat.js
@@ -15,6 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     });
 
+    /************BIND 'ADD' with "Return key"****************** */ 
+    var input = document.getElementById("channel-id");
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("add-channel-button").click();
+        // focus input field
+        document.getElementById("channel-id").focus();
+    }
+    });
+
     /************ AJAX CALL FOR CHANNELS ****************** */ 
     document.querySelector("#add-channel-button").onclick = () => {
 
